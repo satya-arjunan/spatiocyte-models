7388a6dc03c66e52f00fa44cbae398effb45c294
satya-arjunan <satya.arjunan@gmail.com>
2014-10-06 15:59:06 +0900

    Added scripts for smoldyn benchmark

--- benchmark/smoldyn/plot.py
+++ benchmark/smoldyn/plot.py
@@ -21,11 +21,7 @@ def plot_data(N, T, fmt):
     loglog(N, mean, fmt)
 
 
-imp.load_source('spatiocyte_out', 'spatiocyte/spatiocyte_out.py')
-from spatiocyte_out import *
-imp.load_source('egfrd_out', 'egfrd/egfrd_out.py')
-from egfrd_out import *
-imp.load_source('smoldyn_out', 'smoldyn/smoldyn_out.py')
+imp.load_source('smoldyn_out', 'smoldyn_out.py')
 from smoldyn_out import *
 
 from run_all import Nv
@@ -33,18 +29,15 @@ from run_all import Nv
 axes([.12,.14,.86,.83])
 
 X = numpy.array(Nv)
-plot_data(Nv, spatiocyte_data,'r.')
+plot_data(Nv, smoldyn_data,'r.')
 loglog(X, 0.4*X, 'r-')
-
-plot_data(Nv, egfrd_data,'b.')
-loglog(X, 0.1*X, 'b-')
-
-plot_data(Nv, smoldyn_data,'g.')
-loglog(X, 0.1*X, 'g-')
 
 xlabel('N [# particles]', size=22)
 
 Y = numpy.array([60,3600,3600*24,3600*24*30, 3600*24*30*12])
+
+xlim(X[0],X[len(X)-1]*2)
+ylim(0,Y[3])
 
 xticks(size=18)
 yticks(Y, ['minute', 'hour', 'day', 'month', 'year'], size=16)
