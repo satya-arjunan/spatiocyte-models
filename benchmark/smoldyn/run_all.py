2646293eeceffe89333805a341dd58b7924a4237
Satya Arjunan <satya.arjunan@gmail.com>
2014-10-09 13:17:11 +0900

    Smoldyn improved log

--- benchmark/smoldyn/run_all.py
+++ benchmark/smoldyn/run_all.py
@@ -26,7 +26,7 @@ Rv = 2.5e-9 #voxel radius
 Dv = 1e-12 #diffusion coefficient
 Vv = [3e-18, ] * 11 #simulation volume in m^3
 Nv = [100,300,1000,3000,10000,30000,100000,300000,1000000,3000000,10000000] #number of molecules
-Tv = [max(1e-5, min(T, 1e0 / math.pow(N, 2.0 / 3.0))) for N in Nv] #duration
+Tv = [max(1e-5, min(T, 20e0 / math.pow(N, 2.0 / 3.0))) for N in Nv] #duration
 REPEAT = 1
 
 if __name__ == '__main__':
