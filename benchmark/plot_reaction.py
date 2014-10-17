327d172ac9b6bcfc4550981c02c3de1f02a84f3e
satya-arjunan <satya.arjunan@gmail.com>
2014-10-17 16:12:21 +0900

    Small changes

--- benchmark/plot_reaction.py
+++ benchmark/plot_reaction.py
@@ -9,23 +9,23 @@ labelFontSize = 15
 legendFontSize = 14
 lineFontSize = 15
 
-fileNames = ['spatiocyte/ode.csv','egfrd/egfrd.csv', 'egfrd/egfrd_small_r.csv','smoldyn/smoldyn.csv','smoldyn/smoldyn_small_dt.csv','spatiocyte/spatiocyte.csv','spatiocyte/spatiocyte_small_dt.csv']
-legendTitles = ['Mass Action', 'eGFRD ($r=10\ \mathrm{nm},\ \mathrm{RT}=2897\ \mathrm{s}$)','eGFRD ($r=1\ \mathrm{nm},\ \mathrm{RT}=2232\ \mathrm{s}$)','Smoldyn ($\Delta t=1\ \mathrm{ms},\ \mathrm{RT}=17\ \mathrm{s}$)','Smoldyn ($\Delta t=67\ \mathrm{\mu s},\ \mathrm{RT}=243\ \mathrm{s}$)','Spatiocyte ($\Delta t=1\ \mathrm{ms},\ r=38.73\ \mathrm{nm},\ \mathrm{RT}=11\ \mathrm{s}$)','Spatiocyte ($\Delta t=67\ \mathrm{\mu s},\ r=10\ \mathrm{nm},\ \mathrm{RT}=288\ \mathrm{s}$)']
+fileNames = ['egfrd/egfrd.csv', 'egfrd/egfrd_small_r.csv','smoldyn/smoldyn.csv','smoldyn/smoldyn_small_dt.csv','spatiocyte/spatiocyte.csv','spatiocyte/spatiocyte_small_dt.csv','spatiocyte/ode.csv',]
+legendTitles = ['eGFRD ($r=10\ \mathrm{nm},\ \mathrm{RT}=2897\ \mathrm{s}$)','eGFRD ($r=1\ \mathrm{nm},\ \mathrm{RT}=2232\ \mathrm{s}$)','Smoldyn ($\Delta t=1\ \mathrm{ms},\ \mathrm{RT}=17\ \mathrm{s}$)','Smoldyn ($\Delta t=67\ \mathrm{\mu s},\ \mathrm{RT}=243\ \mathrm{s}$)','Spatiocyte ($\Delta t=1\ \mathrm{ms},\ r=38.73\ \mathrm{nm},\ \mathrm{RT}=11\ \mathrm{s}$)','Spatiocyte ($\Delta t=67\ \mathrm{\mu s},\ r=10\ \mathrm{nm},\ \mathrm{RT}=288\ \mathrm{s}$)','Mass Action']
 speciesList = ['E','S','ES','P']
-lines = ['-','-','--','-','--','-','--']
-colors = ['k', 'b', 'b', 'r', 'r', 'g', 'g']
+lines = ['-','--','-','--','-','--','-']
+colors = ['b', 'b', 'r', 'r', 'g', 'g','k']
 
 for f in range(len(fileNames)):
   deli = ','
-  if (f == 3 or f == 4):
+  if (f == 2 or f == 3):
     deli = ' '
   data = genfromtxt(fileNames[f], delimiter=deli).T
   colSize = len(data)-1
   for i in range(colSize):
     if (i == 0):
-      plot(data[0], data[i+1], ls=lines[f], color=colors[f], label=legendTitles[f], linewidth=1)
+      plot(data[0], data[i+1], ls=lines[f], color=colors[f], label=legendTitles[f], linewidth=1.5)
     else:
-      plot(data[0], data[i+1], ls=lines[f], color=colors[f], linewidth=1)
+      plot(data[0], data[i+1], ls=lines[f], color=colors[f], linewidth=1.5)
 
 
 annotate('ES', xy=(90, 0),  xycoords='data', xytext=(-29, 10), textcoords='offset points', color='k', size=16)
@@ -37,7 +37,7 @@ annotate('P', xy=(90, 230),  xycoords='data', xytext=(-27, 12), textcoords='offs
 annotate('S', xy=(90, 680),  xycoords='data', xytext=(-27, 12), textcoords='offset points', color='k', size=16)
 
 ax = gca()
-leg = legend(loc=(0.02,0.225), labelspacing=0.3, handletextpad=0.2, framealpha=0.5)
+leg = legend(loc=(0.02,0.225), labelspacing=0.3, handletextpad=0.2)
 for t in leg.get_texts():
   t.set_fontsize(legendFontSize)   
 xticks(size=17)
