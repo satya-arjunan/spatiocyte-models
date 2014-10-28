774f7597019d5987a52b8967e2dc8a9f521c51dc
satya-arjunan <satya.arjunan@gmail.com>
2014-10-28 14:17:50 +0900

    Updates to plots to use latex typesetting

--- benchmark/plot_diffusion.py
+++ benchmark/plot_diffusion.py
@@ -6,7 +6,19 @@ import numpy
 import imp
 import scipy.io
 from matplotlib.pylab import *
-matplotlib.rcParams["mathtext.fontset"] = "stix"
+#uncomment the following to create valid eps (scribus) and svg (inkscape):
+#rc('svg', embed_char_paths=True)
+#rc('mathtext', fontset=r'stixsans')
+
+#matplotlib.rcParams["mathtext.fontset"] = "stix"
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
+matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage{amsmath}')
+matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage[helvet]{sfmath}')
+
+labelFontSize = 15
+legendFontSize = 14
+lineFontSize = 15
 
 def plot_data(N, T, fmt):
     T = numpy.array(T)
@@ -41,28 +53,31 @@ X = numpy.array(Nv)
 
 plot(Nv, egfrd_dense_data,'bs', label=r'eGFRD ($V=3\ \mathrm{\mu m}^{3}$)')
 loglog(X, 10e-4*X**2.15, 'b-')
-annotate(r'$\mathsf{t} \ \propto \ \mathsf{N}^{\mathsf{2.15}}$', xy=(X[5], egfrd_dense_data[5][0]),  xycoords='data', xytext=(-50, 5), textcoords='offset points', color='b', size=16)
+annotate(r'$RT\propto N^{\mathsf{2.15}}$', xy=(X[5], egfrd_dense_data[5][0]),  xycoords='data', xytext=(10, -2), textcoords='offset points', color='b', size=14)
 
 plot(Nv, egfrd_data,'bo', label=r'eGFRD ($V=3000\ \mathrm{\mu m}^{3}$)')
 loglog(X, 1.1e-4*X**1.75, 'b-')
-annotate(r'$\mathsf{t} \ \propto \ \mathsf{N}^{\mathsf{1.75}}$', xy=(X[3], egfrd_data[3][0]),  xycoords='data', xytext=(-10, -20), textcoords='offset points', color='b', size=16)
+annotate(r'$RT\propto N^{\mathsf{1.75}}$', xy=(X[3], egfrd_data[3][0]),  xycoords='data', xytext=(-10, -20), textcoords='offset points', color='b', size=14)
 
 plot(Nv, smoldyn_data,'g^', label=r'Smoldyn ($V=3\ \mathrm{\mu m}^{3}$)')
 loglog(X, 8e-2*X**1.15, 'g-')
-annotate(r'$\mathsf{t} \ \propto \ \mathsf{N}^{\mathsf{1.15}}$', xy=(X[1], smoldyn_data[1][0]),  xycoords='data', xytext=(-30, -25), textcoords='offset points', color='g', size=16)
+annotate(r'$RT\propto N^{\mathsf{1.15}}$', xy=(X[1], smoldyn_data[1][0]),  xycoords='data', xytext=(-40, -25), textcoords='offset points', color='g', size=14)
 plot(Nv, smoldyn_dillute_data,'gv', label=r'Smoldyn ($V=30\ \mathrm{\mu m}^{3}$)')
 
 plot(Nv, spatiocyte_data,'r<', label=r'Spatiocyte ($V=3\ \mathrm{\mu m}^{3}$)')
 loglog(X, 0.24*X**1.05, 'r-')
-annotate(r'$\mathsf{t} \ \propto \ \mathsf{N}^{\mathsf{1.05}}$', xy=(X[4], spatiocyte_data[4][0]),  xycoords='data', xytext=(-25, 7), textcoords='offset points', color='r', size=16)
+annotate(r'$RT\propto N^{\mathsf{1.05}}$', xy=(X[4], spatiocyte_data[4][0]),  xycoords='data', xytext=(-30, 9), textcoords='offset points', color='r', size=14)
 plot(Nv, spatiocyte_dillute_data,'r>', label=r'Spatiocyte ($V=30\ \mathrm{\mu m}^{3}$)')
 
+annotate('B', xy=(0, 1),  xycoords='axes fraction', xytext=(-60,-20), textcoords='offset points', color='k', size=30)
 
-legend(loc='upper left', labelspacing=0.3, handletextpad=0.2)
+leg = legend(loc='upper left', labelspacing=0.3, handletextpad=0.2)
+for t in leg.get_texts():
+  t.set_fontsize(legendFontSize) 
 #legend(loc='upper left', labelspacing=0.2, handletextpad=0.2, fancybox=True)
 
-xlabel('N (# Molecules)', size=17)
-ylabel('Runtime, RT', size=17)
+xlabel('N (\# Molecules)', size=17)
+ylabel('Runtime, $RT$', size=17)
 
 Y = numpy.array([60,3600,3600*24,3600*24*30, 3600*24*30*12])
 
--- benchmark/plot_reaction.py
+++ benchmark/plot_reaction.py
@@ -3,14 +3,22 @@ import csv
 import math
 from matplotlib import rc
 from pylab import *
-matplotlib.rcParams["mathtext.fontset"] = "stix"
+#uncomment the following to create valid eps (scribus) and svg (inkscape):
+#rc('svg', embed_char_paths=True)
+#rc('mathtext', fontset=r'stixsans')
+
+#matplotlib.rcParams["mathtext.fontset"] = "stix"
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
+matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage{amsmath}')
+matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage[helvet]{sfmath}')
 
 labelFontSize = 15
 legendFontSize = 14
 lineFontSize = 15
 
 fileNames = ['egfrd/egfrd.csv', 'egfrd/egfrd_small_r.csv','smoldyn/smoldyn.csv','smoldyn/smoldyn_small_dt.csv','spatiocyte/spatiocyte.csv','spatiocyte/spatiocyte_small_dt.csv','spatiocyte/ode.csv',]
-legendTitles = ['eGFRD ($r=10\ \mathrm{nm},\ \mathrm{RT}=2897\ \mathrm{s}$)','eGFRD ($r=1\ \mathrm{nm},\ \mathrm{RT}=2232\ \mathrm{s}$)','Smoldyn ($\Delta t=1\ \mathrm{ms},\ \mathrm{RT}=17\ \mathrm{s}$)','Smoldyn ($\Delta t=67\ \mathrm{\mu s},\ \mathrm{RT}=243\ \mathrm{s}$)','Spatiocyte ($\Delta t=1\ \mathrm{ms},\ r=38.73\ \mathrm{nm},\ \mathrm{RT}=11\ \mathrm{s}$)','Spatiocyte ($\Delta t=67\ \mathrm{\mu s},\ r=10\ \mathrm{nm},\ \mathrm{RT}=254\ \mathrm{s}$)','Mass Action']
+legendTitles = ['eGFRD ($r=10\ \mathrm{nm},\ RT=2897\ \mathrm{s}$)','eGFRD ($r=1\ \mathrm{nm},\ RT=2232\ \mathrm{s}$)','Smoldyn ($\Delta t=1\ \mathrm{ms},\ RT=17\ \mathrm{s}$)','Smoldyn ($\Delta t=67\ \mathrm{\mu s},\ RT=243\ \mathrm{s}$)','Spatiocyte ($\Delta t=1\ \mathrm{ms},\ r=38.73\ \mathrm{nm},\ RT=11\ \mathrm{s}$)','Spatiocyte ($\Delta t=67\ \mathrm{\mu s},\ r=10\ \mathrm{nm},\ RT=254\ \mathrm{s}$)','Mass Action']
 speciesList = ['E','S','ES','P']
 lines = ['-','--','-','--','-','--','-']
 colors = ['b', 'b', 'g', 'g', 'r', 'r','k']
@@ -35,16 +43,18 @@ annotate('P', xy=(90, 230),  xycoords='data', xytext=(-27, 12), textcoords='offs
 
 annotate('S', xy=(90, 680),  xycoords='data', xytext=(-27, 12), textcoords='offset points', color='k', size=16)
 
-annotate(r'E + S $k_1 k_2\rightleftharpoons$ ES $k_3\rightarrow$ E + P', xy=(55, 880),  xycoords='data', xytext=(-29, 10), textcoords='offset points', color='k', size=16)
+annotate(r'E + S $\overset{k_1}{\underset{k_2}\rightleftharpoons}$ ES $\overset{k_3}{\rightarrow}$ E + P', xy=(55, 820),  xycoords='data', xytext=(-29, 10), textcoords='offset points', color='k', size=16)
+
+annotate('A', xy=(0, 1),  xycoords='axes fraction', xytext=(-65,-15), textcoords='offset points', color='k', size=30)
 
 ax = gca()
-leg = legend(loc=(0.02,0.23), labelspacing=0.3, handletextpad=0.2)
+leg = legend(loc=(0.02,0.26), labelspacing=0.3, handletextpad=0.2)
 for t in leg.get_texts():
   t.set_fontsize(legendFontSize)   
 xticks(size=17)
 yticks(size=17)
-xlabel('time, t (s)',size=17)
-ylabel('# Molecules',size=17)
+xlabel('time, $t$ (s)',size=17)
+ylabel('\# Molecules',size=17)
 xlim(0,100)
 savefig('reaction.eps', format='eps', dpi=1000)
 show()
