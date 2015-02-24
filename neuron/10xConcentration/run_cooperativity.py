4813de104c150d1ceb2d6c1e5344b835f94541e0
Satya Arjunan <satya.arjunan@gmail.com>
2015-02-24 18:23:29 +0900

    Update

--- neuron/10xConcentration/run_cooperativity.py
+++ neuron/10xConcentration/run_cooperativity.py
@@ -23,7 +23,7 @@ def register_job(param, subproc, subprocs, poller, jobCnt):
   print "started job:", jobCnt, "id:", fd, param
 
 N = 500
-T = 10
+T = 30
 K6 = 1.0
 #K1 = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.02, 0.01, 0.009, 0.008, 0.007, 0.006, 0.005, 0.004, 0.003, 0.002, 0.001, 0.0009, 0.0008, 0.0007, 0.0006, 0.0005, 0.0004, 0.0003, 0.0002, 0.0001, 0.00009, 0.00008, 0.00007, 0.00006, 0.00005, 0.00004, 0.00003, 0.00002, 0.00001, 0.00009, 0.000008, 0.000007, 0.000006, 0.000005, 0.000004, 0.000003, 0.000002, 0.000001]
 K1 = [7.5e-1, 5e-1, 2.5e-1, 1e-1, 7.5e-2, 5e-2, 2.5e-2, 1e-2, 7.5e-3, 5e-3, 2.5e-3, 1e-3, 7.5e-4, 5e-4, 2.5e-4, 1e-4, 7.5e-5, 5e-5, 2.5e-5, 1e-5, 7.5e-6, 5e-6, 2.5e-6, 1e-6]
@@ -37,7 +37,7 @@ if __name__ == '__main__':
         params.append(get_param(T, K1[i], K6, K7[j], K8[k]))
 
   SLICE_IN_SECONDS = 0.1
-  param = get_param(1, K1[0], K6, K7[0], K8[0])
+  param = get_param(0.001, K1[0], K6, K7[0], K8[0])
   subproc = add_job(param)
   resultTable = []
   startTime = time.time()
@@ -48,7 +48,7 @@ if __name__ == '__main__':
   typicalMemory = max(resultTable)*1.7
 
   jobStart = 10
-  jobEnd = 20
+  jobEnd = 200
   print "total jobs:",len(params), "start:", jobStart, "end:", jobEnd
   jobCnt = jobStart
   cpuCnt = psutil.cpu_count()
@@ -56,12 +56,19 @@ if __name__ == '__main__':
   poller = select.epoll()
   subprocs = {}
   doneCnt = 0
-  while (jobCnt != jobEnd and psutil.virtual_memory().available > typicalMemory and jobCnt-jobStart-doneCnt < cpuCnt): 
+  skipCnt = 1
+  while (jobCnt != jobEnd and jobCnt-jobStart-doneCnt < cpuCnt and psutil.virtual_memory().available > typicalMemory):
     #print "avail:",psutil.virtual_memory().available/1024/1024,"req:",typicalMemory/1024/1024
     subproc = add_job(params[jobCnt])
     register_job(params[jobCnt], subproc, subprocs, poller, jobCnt) 
     jobCnt = jobCnt + 1
-    time.sleep(duration)
+    if (psutil.virtual_memory().available > skipCnt*typicalMemory):
+      skipCnt = skipCnt + 1
+      print "init fast",skipCnt,"virt:",psutil.virtual_memory().available/1024/1024,"typ:",skipCnt*typicalMemory/1024/1024
+    else: 
+      skipCnt = 1
+      print "sleep:",skipCnt,"virt:",psutil.virtual_memory().available/1024/1024,"typ:",skipCnt*typicalMemory/1024/1024
+      time.sleep(duration)
   while True:
     for fd, flags in poller.poll(timeout=1):
       done_proc = subprocs[fd]
@@ -73,7 +80,14 @@ if __name__ == '__main__':
         subproc = add_job(params[jobCnt])
         register_job(params[jobCnt], subproc, subprocs, poller, jobCnt) 
         jobCnt = jobCnt + 1
-        time.sleep(duration)
+        if (psutil.virtual_memory().available > skipCnt*typicalMemory):
+          skipCnt = skipCnt + 1
+          print "fast",skipCnt,"virt:",psutil.virtual_memory().available/1024/1024,"typ:",skipCnt*typicalMemory/1024/1024
+        else: 
+          skipCnt = 1
+          print "sleep before break",skipCnt,"virt:",psutil.virtual_memory().available/1024/1024,"typ:",skipCnt*typicalMemory/1024/1024
+          time.sleep(duration)
+          break
     if (doneCnt == jobEnd-jobStart):
       break
 
