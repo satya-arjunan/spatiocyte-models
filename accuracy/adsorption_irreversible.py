f311a4c2d0dd56f7681d35444b1f9cf7a3ce872d
satya-arjunan <satya.arjunan@gmail.com>
2015-03-18 18:42:23 +0900

    fix

--- accuracy/adsorption_irreversible.py
+++ accuracy/adsorption_irreversible.py
@@ -1,5 +1,5 @@
 sim = theSimulator.createStepper('SpatiocyteStepper', 'SS')
-sim.VoxelRadius = 100e-9 
+sim.VoxelRadius = 200e-9 
 sim.SearchVacant = 0
 
 theSimulator.rootSystem.StepperID = 'SS'
@@ -16,24 +16,32 @@ theSimulator.createEntity('Variable', 'Variable:/:YZPLANE').Value = 4
 theSimulator.createEntity('System', 'System:/:Surface').StepperID = 'SS'
 theSimulator.createEntity('Variable', 'Variable:/Surface:DIMENSION').Value = 2
 theSimulator.createEntity('Variable', 'Variable:/Surface:VACANT')
-theSimulator.createEntity('Variable', 'Variable:/Surface:A').Value = 0
+s = theSimulator.createEntity('Variable', 'Variable:/Surface:A')
+s.Value = 0
+s.Name = "HD"
 theSimulator.createEntity('Variable', 'Variable:/:A').Value = 10000
 
 logger = theSimulator.createEntity('VisualizationLogProcess', 'Process:/:logger')
-logger.VariableReferenceList = [['_', 'Variable:/Surface:A'], ['_', 'Variable:/:A']]
+logger.VariableReferenceList = [['_', 'Variable:/:A']]
 logger.LogInterval = 0.01
 
 populator = theSimulator.createEntity('MoleculePopulateProcess', 'Process:/:pop')
 populator.VariableReferenceList = [['_', 'Variable:/:A']]
 
 binder = theSimulator.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:reaction1')
-binder.VariableReferenceList = [['_', 'Variable:/Surface:VACANT','-1']]
 binder.VariableReferenceList = [['_', 'Variable:/:A','-1']]
+binder.VariableReferenceList = [['_', 'Variable:/Surface:VACANT','-1']]
 binder.VariableReferenceList = [['_', 'Variable:/Surface:A','1']]
-binder.p = 1
+binder.k = 1e-6
 
 diffuser = theSimulator.createEntity('DiffusionProcess', 'Process:/:diffuseA')
-diffuser.VariableReferenceList = [['_', 'Variable:/Surface:A']]
+diffuser.VariableReferenceList = [['_', 'Variable:/:A']]
 diffuser.D = 1e-12
 
-run(10)
+logger = theSimulator.createEntity('IteratingLogProcess', 'Process:/:iter')
+logger.VariableReferenceList = [['_', 'Variable:/Surface:A']]
+logger.LogInterval = 1
+logger.LogEnd = 200
+logger.Iterations = 2
+
+run(201)
