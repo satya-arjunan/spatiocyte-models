682dda50c28473317545d0a06c8ae8785cdeb9d7
satya-arjunan <satya.arjunan@gmail.com>
2015-03-18 15:10:17 +0900

    Added adsorption

---
+++ accuracy/adsorption_irreversible.py
@@ -0,0 +1,39 @@
+sim = theSimulator.createStepper('SpatiocyteStepper', 'SS')
+sim.VoxelRadius = 20e-9 
+sim.SearchVacant = 0
+
+theSimulator.rootSystem.StepperID = 'SS'
+theSimulator.createEntity('Variable', 'Variable:/:GEOMETRY').Value = 0
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHX').Value = 100e-6
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHY').Value = 100e-6
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHZ').Value = 100e-6
+theSimulator.createEntity('Variable', 'Variable:/:VACANT')
+theSimulator.createEntity('Variable', 'Variable:/:XYPLANE').Value = 5
+theSimulator.createEntity('Variable', 'Variable:/:XZPLANE').Value = 5
+theSimulator.createEntity('Variable', 'Variable:/:YZPLANE').Value = 4
+
+# Create the surface compartment:
+theSimulator.createEntity('System', 'System:/:Surface').StepperID = 'SS'
+theSimulator.createEntity('Variable', 'Variable:/Surface:DIMENSION').Value = 2
+theSimulator.createEntity('Variable', 'Variable:/Surface:VACANT')
+theSimulator.createEntity('Variable', 'Variable:/Surface:A').Value = 0
+theSimulator.createEntity('Variable', 'Variable:/:A').Value = 10000
+
+logger = theSimulator.createEntity('VisualizationLogProcess', 'Process:/:logger')
+logger.VariableReferenceList = [['_', 'Variable:/Surface:A'], ['_', 'Variable:/:A']]
+logger.LogInterval = 0.01
+
+populator = theSimulator.createEntity('MoleculePopulateProcess', 'Process:/:pop')
+populator.VariableReferenceList = [['_', 'Variable:/:A']]
+
+binder = theSimulator.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:reaction1')
+binder.VariableReferenceList = [['_', 'Variable:/:A','-1']]
+binder.VariableReferenceList = [['_', 'Variable:/Surface:VACANT','-1']]
+binder.VariableReferenceList = [['_', 'Variable:/Surface:A','1']]
+binder.k = 5e-6
+
+diffuser = theSimulator.createEntity('DiffusionProcess', 'Process:/:diffuseA')
+diffuser.VariableReferenceList = [['_', 'Variable:/Surface:A']]
+diffuser.D = 1e-12
+
+run(10)
