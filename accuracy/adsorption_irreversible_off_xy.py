6584229673bd2bf6dafbc978faaaee09dfc2724f
satya-arjunan <satya.arjunan@gmail.com>
2015-06-08 16:37:18 +0900

    Added off_xy

--- accuracy/adsorption_irreversible_off_xy.py
+++ accuracy/adsorption_irreversible_off_xy.py
@@ -5,20 +5,20 @@ sim.SearchVacant = 0
 
 theSimulator.rootSystem.StepperID = 'SS'
 theSimulator.createEntity('Variable', 'Variable:/:GEOMETRY').Value = 0
-theSimulator.createEntity('Variable', 'Variable:/:LENGTHX').Value = 20e-6
-theSimulator.createEntity('Variable', 'Variable:/:LENGTHY').Value = 20e-6
-theSimulator.createEntity('Variable', 'Variable:/:LENGTHZ').Value = 20e-6
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHX').Value = 100e-6
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHY').Value = 100e-6
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHZ').Value = 100e-6
 theSimulator.createEntity('Variable', 'Variable:/:VACANT')
-theSimulator.createEntity('Variable', 'Variable:/:A').Value = 1000
-theSimulator.createEntity('Variable', 'Variable:/:sA').Value = 0
+theSimulator.createEntity('Variable', 'Variable:/:A').Value = 10000
+s = theSimulator.createEntity('Variable', 'Variable:/:sA')
+s.Value = 0
+s.Name = "HD"
 
 logger = theSimulator.createEntity('VisualizationLogProcess', 'Process:/:logger')
 logger.VariableReferenceList = [['_', 'Variable:/:A']]
-logger.VariableReferenceList = [['_', 'Variable:/:sA']]
 
 populator = theSimulator.createEntity('MoleculePopulateProcess', 'Process:/:pop')
 populator.VariableReferenceList = [['_', 'Variable:/:A']]
-populator.VariableReferenceList = [['_', 'Variable:/:sA']]
 
 diffuser = theSimulator.createEntity('DiffusionProcess', 'Process:/:diffuseA')
 diffuser.VariableReferenceList = [['_', 'Variable:/:A']]
@@ -32,13 +32,13 @@ binder = theSimulator.createEntity('DiffusionInfluencedReactionProcess', 'Proces
 binder.VariableReferenceList = [['_', 'Variable:/:A','-1']]
 binder.VariableReferenceList = [['_', 'Variable:/:Vacant','-1']]
 binder.VariableReferenceList = [['_', 'Variable:/:sA','1']]
-binder.p = 0.6
+binder.k = 5e-6
 
 logger = theSimulator.createEntity('IteratingLogProcess', 'Process:/:iter')
 logger.VariableReferenceList = [['_', 'Variable:/:sA']]
 logger.LogInterval = 1
-logger.LogEnd = 20
+logger.LogEnd = 200
 logger.Iterations = 1
 logger.FileName = "IterateLogOffXY.csv"
 
-run(21)
+run(201)
