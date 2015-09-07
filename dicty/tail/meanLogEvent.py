fe3b3e0ba09687e401f1d3b02d96d9108e617822
satya-arjunan <satya.arjunan@gmail.com>
2015-09-07 13:02:58 +0900

    Small update

--- dicty/tail/meanLogEvent.py
+++ dicty/tail/meanLogEvent.py
@@ -2,8 +2,17 @@ import numpy as np
 import sys
 
 filename = sys.argv[1]
-startTime = 10.0
+
+f = open(filename, 'r')
+legendTitles = f.readline().strip().split(",")
+speciesNames = []
+for i in range(len(legendTitles)-2):
+  speciesNames.append(legendTitles[i+2])
+print speciesNames
 
 data = np.genfromtxt(filename, delimiter=',', skip_header=1)
-print np.mean(data, axis=0, dtype=np.float64)
-
+mean = np.mean(data, axis=0, dtype=np.float64)
+cols = mean.shape[0]
+v = mean[2:cols]
+print v
+print "1st order rate:", v[cols-2-1]*v[cols-2-2]
