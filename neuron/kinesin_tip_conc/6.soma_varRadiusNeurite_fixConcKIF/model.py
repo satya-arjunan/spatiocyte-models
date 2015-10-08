b2926e618a48594ff3da0da6b334003148f72335
satya-arjunan <satya.arjunan@gmail.com>
2015-10-08 19:23:09 +0900

    Added

--- neuron/kinesin_tip_conc/6.soma_varRadiusNeurite_fixConcKIF/model.py
+++ neuron/kinesin_tip_conc/6.soma_varRadiusNeurite_fixConcKIF/model.py
@@ -3,7 +3,7 @@ try:
   T
 except NameError:
   T = 4
-  V1 = 10 #percentage increase in neurite radius
+  V1 = 50 #percentage increase in neurite radius
   V2 = 55 #ratchet rate
   V3 = 1.0 #p
 
@@ -18,17 +18,18 @@ filename = "_%d_%d_%.2f" %(int(V1), int(V2), V3)
 neuriteLength = nBin*binLength
 Filaments = 13
 MTRadius = 12.5e-9
-#VoxelRadius = 0.7e-8 (actual value used)
-VoxelRadius = 0.7e-8
+VoxelRadius = 0.7e-8 (actual value used)
+#VoxelRadius = 1.0e-8
 KinesinRadius = 0.4e-8
 neuriteRadius = 0.3e-6
 somaRadius = 2e-6
 nNeurite = 4
 pPlusEnd_Detach = 1
 KinesinConc = 2e-7 #in Molar
-volumes = [1.5531e-17, 1.5659e-17, 1.5788e-17, 1.5916e-17, 1.6045e-17, 1.6173e-17, 1.6302e-17, 1.6430e-17, 1.6558e-17, 1.6687e-17, 1.6815e-17]
+volumes = [1.5691e-17, 1.6931e-17, 1.8196e-17, 1.9486e-17, 2.0801e-17, 2.2141e-17, 2.3506e-17, 2.4895e-17, 2.6304e-17, 2.7741e-17, 2.9199e-17]
 #Volume =  math.pi*pow(neuriteRadius, 2.0)*neuriteLength*nNeurite
-Volume =  volumes[int(V1)/10]
+#Volume =  volumes[int(V1)/10]
+Volume =  volumes[0]
 nKinesin = int(round(KinesinConc*scipy.constants.N_A*1e+3*Volume))
 print "Volume:", Volume, "nKinesin:", nKinesin
 
