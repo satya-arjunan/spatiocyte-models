e8b00be99e0e4fa39c0d717f5f462f0292d19a26
satya-arjunan <satya.arjunan@gmail.com>
2015-10-10 16:22:11 +0900

    Added

---
+++ neuron/kinesin_tip_conc/1.varLengthNeurite_fixConcKIF/data/publish_collision_bar.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# a bar plot with errorbars
+import numpy as np
+import matplotlib.pyplot as plt
+import colorsys
+
+fontsize = 20
+
+def get_colors(num_colors):
+    colors=[]
+    for i in np.arange(0., 360., 360. / num_colors):
+        hue = i/360.
+        lightness = (35 + np.random.rand() * 10)/100.
+        saturation = (70 + np.random.rand() * 10)/100.
+        colors.append(colorsys.hls_to_rgb(hue, lightness, saturation))
+    return colors
+
+kelly_colors = ['#007D34', '#00538A', '#7F180D', '#53377A', '#FF8E00', '#B32851', '#F4C800', '#93AA00', '#593315', '#F13A13', '#232C16', '#FFB300', '#803E75', '#FF6800', '#A6BDD7', '#C10020', '#CEA262', '#817066', '#F6768E', '#FF7A5C']
+
+
+def load_data(file):
+  loaddata = np.loadtxt(file, delimiter=",", dtype=str)
+  rows, cols = loaddata.shape
+  abs_val = loaddata[0][0]
+  data = loaddata[1:rows, 1:cols].astype(float)
+  row_labels = loaddata[1:rows+1, 0:1].reshape(rows-1)
+  col_labels = loaddata[0:1, 1:cols+1].reshape(cols-1)
+  return data, row_labels, col_labels, abs_val
+
+def plot_figure(data, row_labels, col_labels, abs_val, plot_cols, plot_rows):
+  row_headers = np.zeros((len(row_labels), len(row_labels[0].split(" "))))
+  for i in range(len(row_labels)):
+    labels = row_labels[i].split(" ")
+    for j in range(len(labels)):
+      row_headers[i, j] = float(labels[j])
+
+  col_headers = np.zeros((len(col_labels), len(col_labels[0].split(" "))))
+  for i in range(len(col_labels)):
+    labels = col_labels[i].split(" ")
+    for j in range(len(labels)):
+      col_headers[i, j] = float(labels[j])
+  x = row_headers[plot_rows[0]:plot_rows[1], 0:1]
+  rows, cols = data.shape
+  fig, ax = plt.subplots()
+  rects = []
+  interval = (x[1]-x[0])[0]
+  pcols = plot_cols[1]-plot_cols[0]+1
+  width = interval/(pcols+1.0)
+  ax.set_color_cycle(['r', 'b', 'g', 'a'])
+  colors = get_colors(pcols)
+  for i in range(pcols):
+    y = data[plot_rows[0]:plot_rows[1], plot_cols[0]+i-1:plot_cols[0]+i]
+    xpos = -interval/2.0+width/2.0+i*width
+    rects.append(ax.bar(x+xpos, y, width, color=colors[i])[0])
+  ax.set_xlim(x[0]-interval/2.0, x[-1]+interval/2.0)
+  ax.set_ylabel('Cytosolic kinesin tip concentration\n(% of max)',
+      fontsize=fontsize)
+  ax.set_xlabel('Neurite length (x0.4 um)', fontsize=fontsize)
+  ax.set_xticks(x)
+  ax.tick_params(axis='both', which='major', labelsize=fontsize)
+  ax.tick_params(axis='both', which='minor', labelsize=fontsize)
+  #ax.set_xticklabels(x, fontsize=20)
+  legends = []
+  for i in range(pcols):
+    legends.append("p=%.1f" %col_headers[plot_cols[0]+i-1])
+  ax.legend(rects, legends, loc='upper left', title="MT binding\nprobability",
+      fontsize=fontsize)
+
+  ax.get_legend().get_title().set_fontsize('20')
+  plt.show()
+
+file = "saved_data.csv"
+data, row_labels, col_labels, abs_val = load_data(file)
+plot_cols = [4, 6]
+plot_rows = [11, 20]
+plot_figure(data, row_labels, col_labels, abs_val, plot_cols, plot_rows)
+
+
+
+
