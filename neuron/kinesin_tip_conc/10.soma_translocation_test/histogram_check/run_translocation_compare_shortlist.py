8149a7145c830accfbd0e38f9d01cdc0f6719dc5
Satya Arjunan <satya.arjunan@gmail.com>
2015-10-14 20:29:48 +0900

    Add

---
+++ neuron/kinesin_tip_conc/10.soma_translocation_test/histogram_check/run_translocation_compare_shortlist.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+
+labelFontSize = 18
+tickFontSize = 18
+legendFontSize = 18
+lineFontSize = 18
+lines = ['-', '-', '-', '-']
+colors = ['r', 'g', 'b', 'y', 'c', 'k']
+legendTitles = ['7um Neurite 1', '7um Neurite 2', '7um Neurite 3', '7um Neurite 4']
+
+def get_bins(file, start_time, end_time):
+  data = np.loadtxt(file, delimiter=',', skiprows=1)
+  bins = 0
+  initTime = float(data[0][0])
+  for row in data:
+    if(row[0] == initTime):
+      bins = bins+1
+  logInterval = data[bins][0]-initTime
+  start_row = int(np.ceil((start_time-initTime)/logInterval))*bins
+  drows = int((end_time-start_time)/logInterval*bins)
+  return bins, start_row, drows
+
+def initialize(start_time, end_time, bin_no):
+  filenames = np.transpose(sorted(glob.glob("histogram_*.csv")))
+  print filenames
+  for i in range(len(filenames)/4):
+    print "i:",i
+    fig, ax = plt.subplots()
+    for j in range(4):
+      file = filenames[i*4+j]
+      print file, i, j, i*4+j
+      bins, start_row, drows = get_bins(file, start_time, end_time)
+      data = np.loadtxt(file, delimiter=",", skiprows=start_row+1)
+      rows,cols = data.shape
+      xcol = 0 #time
+      ycols = [cols-5, cols-4, cols-3, cols-2, cols-1]
+      x = np.divide(data[0:drows, xcol:xcol+1].reshape(drows/bins, bins), 3600.0)
+      y = np.zeros((drows/bins, 1))
+      for k in ycols:
+        tmp = data[0:drows, k:k+1].reshape(drows/bins, bins)[:, bin_no-1:bin_no]
+        y = np.add(y, tmp)
+      ax.plot(x, y)
+  plt.show()
+
+initialize(1000, 1000+3600*10, -1)
