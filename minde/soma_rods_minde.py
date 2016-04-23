c68306d8ef0a28b4aa390c36e9ff83f719e33c11
satya-arjunan <satya.arjunan@gmail.com>
2016-04-23 17:29:15 +0900

    Added

---
+++ minde/soma_rods_minde.py
@@ -0,0 +1,268 @@
+
+try:
+  T
+except NameError:
+  T = 400
+  V1 = 0 #extra nBin in one rod
+  V2 = 55 #ratchet rate
+  V3 = 1.0 #p
+
+import math
+import scipy.constants
+import numpy as np
+
+interval = 1
+
+nBinX = int(V1)
+nBin = 10+nBinX
+binLength = 0.2e-6
+filename = "_%d_%d_%.2f" %(int(V1), int(V2), V3)
+rodLength = nBin*binLength
+VoxelRadius = 1e-8
+rodRadius = 0.5e-6
+somaRadius = 0.8e-6
+nRod = 3
+KinesinConc = 2e-7 #in Molar
+volumes = [1.6647e-17, 1.6796e-17, 1.6940e-17, 1.7088e-17, 1.7236e-17, 1.7384e-17, 1.7528e-17, 1.7676e-17, 1.7821e-17, 1.7969e-17, 1.8113e-17]
+Volume =  volumes[int(V1)]
+nKinesin = int(round(KinesinConc*scipy.constants.N_A*1e+3*Volume))
+print "Volume:", Volume, "nKinesin:", nKinesin
+
+def rotatePointAlongVector(P, C, N, angle):
+  x = P[0]
+  y = P[1]
+  z = P[2]
+  a = C[0]
+  b = C[1]
+  c = C[2]
+  u = N[0]
+  v = N[1]
+  w = N[2]
+  u2 = u*u
+  v2 = v*v
+  w2 = w*w
+  cosT = math.cos(angle)
+  oneMinusCosT = 1-cosT
+  sinT = math.sin(angle)
+  xx = (a*(v2+w2)-u*(b*v+c*w-u*x-v*y-w*z))*oneMinusCosT+x*cosT+(
+      -c*v+b*w-w*y+v*z)*sinT
+  yy = (b*(u2+w2)-v*(a*u+c*w-u*x-v*y-w*z))*oneMinusCosT+y*cosT+(
+      c*u-a*w+w*x-u*z)*sinT
+  zz = (c*(u2+v2)-w*(a*u+b*v-u*x-v*y-w*z))*oneMinusCosT+z*cosT+(
+      -b*u+a*v-v*x+u*y)*sinT
+  return [xx, yy, zz]
+
+angle = math.pi/nRod
+vectorZ = [0.0, 0.0, 1.0]
+vectorZpoint = [0.0, 0.0, 0.0]
+inSomaLength = VoxelRadius*10
+rodsLengthX = [rodLength-nBinX*binLength]*nRod
+rodsLengthX[nRod-1] = rodLength #longer rod
+rodsRotateZ = np.zeros(nRod)
+rodsOrigin = np.zeros((nRod, 3))
+maxPoint = np.full(3, -np.inf)
+minPoint = np.full(3, np.inf)
+for i in range(nRod):
+  tip = somaRadius+rodsLengthX[i]-inSomaLength+rodRadius*2
+  mid = somaRadius+rodsLengthX[i]/2-inSomaLength
+  rad = angle+angle*2*i
+  rodsRotateZ[i] = -rad
+  origin = [mid, 0, 0]
+  rodsOrigin[i] = rotatePointAlongVector(origin, vectorZpoint, vectorZ, rad)
+  edge = [tip, 0, 0]
+  point = rotatePointAlongVector(edge, vectorZpoint, vectorZ, rad)
+  maxPoint = np.amax([maxPoint, point], axis=0)
+  minPoint = np.amin([minPoint, point], axis=0)
+
+rootLengths = np.subtract(maxPoint, minPoint)
+halfRootLengths = np.divide(rootLengths, 2.0)
+center = np.subtract(0, np.add(halfRootLengths, minPoint))
+with np.errstate(divide='ignore', invalid='ignore'):
+  somaOrigin = np.true_divide(center, halfRootLengths)
+  somaOrigin[somaOrigin == np.inf] = 0
+  somaOrigin = np.nan_to_num(somaOrigin)
+for i in range(nRod):
+  with np.errstate(divide='ignore', invalid='ignore'):
+    rodsOrigin[i] = np.divide(rodsOrigin[i], halfRootLengths)
+    rodsOrigin[i][rodsOrigin[i] == np.inf] = 0
+    rodsOrigin[i] = np.nan_to_num(rodsOrigin[i])
+  rodsOrigin[i] = np.add(rodsOrigin[i], somaOrigin)
+
+sim = theSimulator
+sim.createStepper('SpatiocyteStepper', 'SS').VoxelRadius = VoxelRadius
+sim.rootSystem.StepperID = 'SS'
+
+sim.createEntity('Variable', 'Variable:/:LENGTHX').Value = rootLengths[0]
+sim.createEntity('Variable', 'Variable:/:LENGTHY').Value = rootLengths[1]
+sim.createEntity('Variable', 'Variable:/:LENGTHZ').Value = rodRadius*4+6*VoxelRadius
+sim.createEntity('Variable', 'Variable:/:VACANT')
+
+sim.createEntity('System', 'System:/:Soma').StepperID = 'SS'
+sim.createEntity('Variable', 'Variable:/Soma:GEOMETRY').Value = 1
+sim.createEntity('Variable', 'Variable:/Soma:LENGTHX').Value = somaRadius*2
+sim.createEntity('Variable', 'Variable:/Soma:LENGTHY').Value = somaRadius*2
+sim.createEntity('Variable', 'Variable:/Soma:LENGTHZ').Value = somaRadius*2
+sim.createEntity('Variable', 'Variable:/Soma:ORIGINX').Value = somaOrigin[0]
+sim.createEntity('Variable', 'Variable:/Soma:ORIGINY').Value = somaOrigin[1]
+sim.createEntity('Variable', 'Variable:/Soma:ORIGINZ').Value = somaOrigin[2]
+sim.createEntity('Variable', 'Variable:/Soma:VACANT').Value = -1
+
+sim.createEntity('System', 'System:/Soma:Surface').StepperID = 'SS'
+sim.createEntity('Variable', 'Variable:/Soma/Surface:DIMENSION').Value = 2
+sim.createEntity('Variable', 'Variable:/Soma/Surface:VACANT')
+
+for i in range(nRod):
+  sim.createEntity('System', 'System:/:Rod%d' %i).StepperID = 'SS'
+  sim.createEntity('Variable', 'Variable:/Rod%d:GEOMETRY' %i).Value = 2
+  x = sim.createEntity('Variable', 'Variable:/Rod%d:LENGTHX' %i)
+  x.Value = rodsLengthX[i]
+  y = sim.createEntity('Variable', 'Variable:/Rod%d:LENGTHY' %i)
+  y.Value = rodRadius*2
+  x = sim.createEntity('Variable', 'Variable:/Rod%d:ORIGINX' %i)
+  x.Value = rodsOrigin[i][0]
+  y = sim.createEntity('Variable', 'Variable:/Rod%d:ORIGINY' %i)
+  y.Value = rodsOrigin[i][1]
+  sim.createEntity('Variable', 'Variable:/Rod%d:ORIGINZ' %i).Value = 0
+  r = sim.createEntity('Variable', 'Variable:/Rod%d:ROTATEZ' %i)
+  r.Value = rodsRotateZ[i]
+  sim.createEntity('Variable', 'Variable:/Rod%d:VACANT' %i)
+  d = sim.createEntity('Variable', 'Variable:/Rod%d:DIFFUSIVE' %i)
+  d.Name = '/:Soma'
+  # Create the rod membrane:
+  sim.createEntity('System', 'System:/Rod%d:Surface' %i).StepperID = 'SS'
+  sim.createEntity('Variable',
+      'Variable:/Rod%d/Surface:DIMENSION' %i).Value = 2
+  sim.createEntity('Variable', 'Variable:/Rod%d/Surface:VACANT' %i)
+  sim.createEntity('Variable',
+      'Variable:/Rod%d/Surface:DIFFUSIVE' %i).Name = '/Soma:Surface'
+
+  h = sim.createEntity('HistogramLogProcess', 'Process:/Rod%d:Histogram' %i)
+  h.VariableReferenceList = [['_', 'Variable:/Soma:MinDatp' ]]
+  h.VariableReferenceList = [['_', 'Variable:/Soma:MinDadp' ]]
+  h.VariableReferenceList = [['_', 'Variable:/Soma:MinEE' ]]
+  h.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD' ]]
+  h.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEED' ]]
+  h.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEE' ]]
+  h.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinEE' ]]
+#  if(i == 2):
+#    h.VariableReferenceList = [['_', 'Variable:/Soma/Surface:A', '-2']]
+#    h.VariableReferenceList = [['_', 'Variable:/Soma/Surface:C', '-3']]
+#    h.VariableReferenceList = [['_', 'Variable:/Soma:B', '-1']]
+  h.Density = 1
+  h.Length = rodsLengthX[i]
+  h.Radius = rodRadius*1.5
+  h.Bins = int(round(rodsLengthX[i]/binLength))/2 
+  h.LogInterval = interval/10.0
+  h.ExposureTime = interval
+  h.FileName = "histogram" + filename + ("_n%d.csv" %i)
+  h.LogEnd = T-1
+  h.Iterations = 1
+
+sim.createEntity('Variable', 'Variable:/Soma:MinDatp').Value = 2706
+sim.createEntity('Variable', 'Variable:/Soma:MinDadp').Value = 0
+sim.createEntity('Variable', 'Variable:/Soma:MinEE').Value = 0
+sim.createEntity('Variable', 'Variable:/Soma:B').Value = 0
+
+d = sim.createEntity('DiffusionProcess', 'Process:/Soma:diffuseMinDatp')
+d.VariableReferenceList = [['_', 'Variable:/Soma:MinDatp']]
+d.D = 16e-12
+
+d = sim.createEntity('DiffusionProcess', 'Process:/Soma:diffuseMinDadp')
+d.VariableReferenceList = [['_', 'Variable:/Soma:MinDadp']]
+d.D = 16e-12
+
+d = sim.createEntity('DiffusionProcess', 'Process:/Soma:diffuseMinE')
+d.VariableReferenceList = [['_', 'Variable:/Soma:MinEE']]
+d.D = 10e-12
+
+l = sim.createEntity('VisualizationLogProcess', 'Process:/Soma:logger')
+l.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinEE']]
+l.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEE']]
+l.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEED']]
+l.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD']]
+l.VariableReferenceList = [['_', 'Variable:/Soma/Surface:A']]
+l.VariableReferenceList = [['_', 'Variable:/Soma/Surface:C']]
+l.VariableReferenceList = [['_', 'Variable:/Soma:B']]
+l.LogInterval = 0.5
+
+p = sim.createEntity('MoleculePopulateProcess', 'Process:/Soma:pop')
+p.VariableReferenceList = [['_', 'Variable:/Soma:MinDatp']]
+p.VariableReferenceList = [['_', 'Variable:/Soma:MinDadp']]
+p.VariableReferenceList = [['_', 'Variable:/Soma:MinEE']]
+p.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinEE']]
+p.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEE']]
+p.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEED']]
+p.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD']]
+
+
+sim.createEntity('Variable', 'Variable:/Soma/Surface:MinD').Value = 0
+sim.createEntity('Variable', 'Variable:/Soma/Surface:MinEE').Value = 0
+sim.createEntity('Variable', 'Variable:/Soma/Surface:MinDEE').Value = 1457
+sim.createEntity('Variable', 'Variable:/Soma/Surface:MinDEED').Value = 0
+sim.createEntity('Variable', 'Variable:/Soma/Surface:A').Value = 0
+sim.createEntity('Variable', 'Variable:/Soma/Surface:C').Value = 0
+
+diffuser = sim.createEntity('DiffusionProcess', 'Process:/:diffuseMinD')
+diffuser.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD']]
+diffuser.D = 0.02e-12
+
+diffuser = sim.createEntity('DiffusionProcess', 'Process:/:diffuseMinEE')
+diffuser.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinEE']]
+diffuser.D = 0.02e-12
+
+diffuser = sim.createEntity('DiffusionProcess', 'Process:/:diffuseMinDEE')
+diffuser.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEE']]
+diffuser.D = 0.02e-12
+
+diffuser = sim.createEntity('DiffusionProcess', 'Process:/:diffuseMinDEED')
+diffuser.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEED']]
+diffuser.D = 0.02e-12
+
+r = sim.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:r1')
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:VACANT','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma:MinDatp','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD','1']]
+r.k = 2.2e-8
+
+r = sim.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:r2')
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma:MinDatp','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD','1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD','1']]
+r.k = 3e-20
+
+r = sim.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:r3')
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma:MinEE','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEE','1']]
+r.k = 5e-19
+
+r = sim.createEntity('SpatiocyteNextReactionProcess', 'Process:/:r4')
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEE','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinEE','1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma:MinDatp','1']]
+r.k = 1
+
+r = sim.createEntity('SpatiocyteNextReactionProcess', 'Process:/:r5')
+r.VariableReferenceList = [['_', 'Variable:/Soma:MinDadp','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma:MinDatp','1']]
+r.k = 5
+
+r = sim.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:r6')
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEE','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinD','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEED','1']]
+r.k = 5e-15
+
+r = sim.createEntity('SpatiocyteNextReactionProcess', 'Process:/:r7')
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEED','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinDEE','1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma:MinDadp','1']]
+r.k = 1
+
+r = sim.createEntity('SpatiocyteNextReactionProcess', 'Process:/:r8')
+r.VariableReferenceList = [['_', 'Variable:/Soma/Surface:MinEE','-1']]
+r.VariableReferenceList = [['_', 'Variable:/Soma:MinEE','1']]
+r.k = 0.83
+
+run(T)
--- tmp/ode-snrp-particle.py
+++ tmp/ode-snrp-particle.py
@@ -4,15 +4,16 @@ s.VoxelRadius = 4.4e-9
 s = sim.createStepper('ODEStepper', 'DE')
 s.MaxStepInterval = 1e-3
 sim.rootSystem.StepperID = 'SS'
+
 sim.createEntity('Variable', 'Variable:/:GEOMETRY').Value = 0
 sim.createEntity('Variable', 'Variable:/:LENGTHX').Value = 5e-7
 sim.createEntity('Variable', 'Variable:/:LENGTHY').Value = 5e-7
 sim.createEntity('Variable', 'Variable:/:LENGTHZ').Value = 5e-7
+
 sim.createEntity('Variable', 'Variable:/:VACANT')
 sim.createEntity('Variable', 'Variable:/:A').Value = 1500
 sim.createEntity('Variable', 'Variable:/:B').Value = 0
 sim.createEntity('Variable', 'Variable:/:C').Value = 0
-
 v = sim.createEntity('Variable', 'Variable:/:E')
 v.Value = 100
 v.Name = 'HD'
@@ -26,66 +27,59 @@ v = sim.createEntity('Variable', 'Variable:/:P')
 v.Value = 0
 v.Name = 'HD'
 
-# E + S -> ES
+p = sim.createEntity('MoleculePopulateProcess', 'Process:/:pop')
+p.VariableReferenceList = [['_', 'Variable:/:A']]
+
+d = sim.createEntity('DiffusionProcess', 'Process:/:d1')
+d.VariableReferenceList = [['_', 'Variable:/:A']]
+d.D = 5e-16
+d = sim.createEntity('DiffusionProcess', 'Process:/:d2')
+d.VariableReferenceList = [['_', 'Variable:/:B']]
+d.D = 5e-16
+d = sim.createEntity('DiffusionProcess', 'Process:/:d3')
+d.VariableReferenceList = [['_', 'Variable:/:C']]
+d.D = 5e-16
+
+# E + S --> ES
 r = sim.createEntity('MassActionProcess', 'Process:/:r1')
 r.StepperID = 'DE'
 r.VariableReferenceList = [['_', 'Variable:.:E','-1']]
 r.VariableReferenceList = [['_', 'Variable:.:S','-1']]
 r.VariableReferenceList = [['_', 'Variable:.:ES','1']]
 r.k = 1e-22
-
-# ES -> E + S
+# ES --> E + S
 r = sim.createEntity('MassActionProcess', 'Process:/:r2')
 r.StepperID = 'DE'
 r.VariableReferenceList = [['_', 'Variable:.:ES', '-1']]
 r.VariableReferenceList = [['_', 'Variable:.:E', '1']]
 r.VariableReferenceList = [['_', 'Variable:.:S', '1']]
 r.k = 1e-1
-
-# ES -> E + P
+# ES --> E + P
 r = sim.createEntity('MassActionProcess', 'Process:/:r3')
 r.StepperID = 'DE'
 r.VariableReferenceList = [['_', 'Variable:.:ES', '-1']]
 r.VariableReferenceList = [['_', 'Variable:.:E', '1']]
 r.VariableReferenceList = [['_', 'Variable:.:P', '1']]
 r.k = 1e-1
+# P + A --> B
+r = sim.createEntity('SpatiocyteNextReactionProcess', 'Process:/:r4')
+r.VariableReferenceList = [['_', 'Variable:/:P', '-1']]
+r.VariableReferenceList = [['_', 'Variable:/:A', '-1']]
+r.VariableReferenceList = [['_', 'Variable:/:B', '1']]
+r.k = 5e-24
+# A + B --> C
+r = sim.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:r5')
+r.VariableReferenceList = [['_', 'Variable:/:A', '-1']]
+r.VariableReferenceList = [['_', 'Variable:/:B', '-1']]
+r.VariableReferenceList = [['_', 'Variable:/:C', '1']]
+r.k = 5e-24
 
-# P + A -> B
-s = sim.createEntity('SpatiocyteNextReactionProcess', 'Process:/:r4')
-s.VariableReferenceList = [['_', 'Variable:/:P', '-1']]
-s.VariableReferenceList = [['_', 'Variable:/:A', '-1']]
-s.VariableReferenceList = [['_', 'Variable:/:B', '1']]
-s.k = 5e-24
-
-# A + B -> C
-d = sim.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:r5')
-d.VariableReferenceList = [['_', 'Variable:/:A', '-1']]
-d.VariableReferenceList = [['_', 'Variable:/:B', '-1']]
-d.VariableReferenceList = [['_', 'Variable:/:C', '1']]
-d.k = 5e-24
-
-p = sim.createEntity('MoleculePopulateProcess', 'Process:/:pop')
-p.VariableReferenceList = [['_', 'Variable:/:A']]
-
-d = sim.createEntity('DiffusionProcess', 'Process:/:diffuseA')
-d.VariableReferenceList = [['_', 'Variable:/:A']]
-d.D = 5e-16
-
-d = sim.createEntity('DiffusionProcess', 'Process:/:diffuseB')
-d.VariableReferenceList = [['_', 'Variable:/:B']]
-d.D = 5e-16
-
-d = sim.createEntity('DiffusionProcess', 'Process:/:diffuseC')
-d.VariableReferenceList = [['_', 'Variable:/:C']]
-d.D = 5e-16
-
-l = sim.createEntity('VisualizationLogProcess', 'Process:/:logger')
+l = sim.createEntity('VisualizationLogProcess', 'Process:/:l1')
 l.VariableReferenceList = [['_', 'Variable:/:A']]
 l.VariableReferenceList = [['_', 'Variable:/:B']]
 l.VariableReferenceList = [['_', 'Variable:/:C']]
 l.LogInterval = 1e-1
-
-l = sim.createEntity('IteratingLogProcess', 'Process:/:logiter')
+l = sim.createEntity('IteratingLogProcess', 'Process:/:l2')
 l.VariableReferenceList = [['_', 'Variable:/:A']]
 l.VariableReferenceList = [['_', 'Variable:/:B']]
 l.VariableReferenceList = [['_', 'Variable:/:C']]
@@ -95,5 +89,4 @@ l.VariableReferenceList = [['_', 'Variable:.:ES']]
 l.VariableReferenceList = [['_', 'Variable:.:P']]
 l.LogInterval = 1e-2
 l.LogEnd = 99
-
 run(100)
