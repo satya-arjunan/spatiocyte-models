7ce16b49c7a82de2f4eb1a0ae88c1e0e6ffdff80
Satya Arjunan <satya.arjunan@gmail.com>
2017-09-21 16:58:48 +0900

    Added zero order sturrock

--- 2016.sturrock.phys.biol/model_zero_order_rv0.125.py
+++ 2016.sturrock.phys.biol/model_zero_order_rv0.125.py
@@ -1,17 +1,17 @@
 try:
   T
 except NameError:
-  T = 800
-  Iterations = 1
+  T = 2000
+  Iterations = 5000
 
 import math 
 sim = theSimulator
 dt = 0.1
 s = sim.createStepper('SpatiocyteStepper', 'SS')
-s.VoxelRadius = 0.05
+s.VoxelRadius = 0.125
 s.SearchVacant = 1
 #s.DebugLevel = 0
-nVoxels = 2352
+nVoxels = 384.0
 
 sim.rootSystem.StepperID = 'SS'
 sim.createEntity('Variable', 'Variable:/:GEOMETRY').Value = 0
@@ -23,7 +23,6 @@ sim.createEntity('Variable', 'Variable:/:XZPLANE').Value = 1 #periodic
 sim.createEntity('Variable', 'Variable:/:YZPLANE').Value = 1 #periodic
 sim.createEntity('Variable', 'Variable:/:VACANT')
 sim.createEntity('Variable', 'Variable:/:A').Value = 0
-sim.createEntity('Variable', 'Variable:/:C').Value = nVoxels-1
 sim.createEntity('Variable', 'Variable:/:B').Value = 1
 
 #l = sim.createEntity('VisualizationLogProcess', 'Process:/:logger')
@@ -36,41 +35,29 @@ p.VariableReferenceList = [['_', 'Variable:/:A']]
 
 p = sim.createEntity('MoleculePopulateProcess', 'Process:/:p2')
 p.VariableReferenceList = [['_', 'Variable:/:B']]
-p.UniformLengthX = 0.1
-p.UniformLengthY = 0.1
-p.UniformLengthZ = 0.1
+p.UniformLengthX = 0.3
+p.UniformLengthY = 0.3
+p.UniformLengthZ = 0.3
 p.Priority = 100
-
-p = sim.createEntity('MoleculePopulateProcess', 'Process:/:p3')
-p.VariableReferenceList = [['_', 'Variable:/:C']]
 
 d = sim.createEntity('DiffusionProcess', 'Process:/:diffuseA')
 d.VariableReferenceList = [['_', 'Variable:/:A']]
-d.WalkReact = 1
 d.D = 0.1
-
-b = sim.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:r1')
-b.VariableReferenceList = [['_', 'Variable:/:A','-1']]
-b.VariableReferenceList = [['_', 'Variable:/:C','-1']]
-b.VariableReferenceList = [['_', 'Variable:/:C','1']]
-b.VariableReferenceList = [['_', 'Variable:/:A','1']]
-b.ForcedSequence = 1
-b.p = 1
 
 b = sim.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:r2')
 b.VariableReferenceList = [['_', 'Variable:/:A','-1']]
 b.VariableReferenceList = [['_', 'Variable:/:B','-1']]
-b.VariableReferenceList = [['_', 'Variable:/:C','1']]
+b.VariableReferenceList = [['_', 'Variable:/:VACANT','1']]
 b.VariableReferenceList = [['_', 'Variable:/:B','1']]
 b.ForcedSequence = 1
 #b.k = 0.02
 #b.k = 0.023785
-b.p = 0.02/(6*math.sqrt(2.0)*d.D*2*s.VoxelRadius)
+b.k = 0.02/(1-0.02/(4*math.pi*2*s.VoxelRadius*d.D))
+#b.p = 0.02/(6*math.sqrt(2.0)*d.D*2*s.VoxelRadius)
 
 b = sim.createEntity('SpatiocyteNextReactionProcess', 'Process:/:r3')
-b.VariableReferenceList = [['_', 'Variable:/:C','-1']]
 b.VariableReferenceList = [['_', 'Variable:/:A','1']]
-b.k = 0.1/nVoxels
+b.k = 0.1/4.24264
 
 
 l = sim.createEntity('IteratingLogProcess', 'Process:/:iter')
@@ -78,6 +65,6 @@ l.VariableReferenceList = [['_', 'Variable:/:A']]
 l.LogInterval = dt
 l.LogEnd = T
 l.Iterations = Iterations
-l.Verbose = 0
+l.Verbose = 1
 
-run(T+0.01)
+run(T+1.0)
